d0806fddda0e5940ac7c547bc93df0ce3bb16457
Artem <usenko.job@gmail.com>
2018-11-12 13:31:15 +0200

    Update main.py

--- km-51/Usenko_Artem/workshop2/source/main.py
+++ km-51/Usenko_Artem/workshop2/source/main.py
@@ -8,12 +8,6 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/api/<action>', methods=['GET'])
 def action(action):
     if action == "chatSettings":
